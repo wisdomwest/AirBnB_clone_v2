c5905576296334aa05c40b60d0ee47fd13295ece
wisdomwest <harmonykoome@gmail.com>
2023-11-19 09:02:53 -0800

    damn

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -18,9 +18,8 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', credits='delete')
-    else:
         @property
         def cities(self):
             """returns the list of City instances"""
