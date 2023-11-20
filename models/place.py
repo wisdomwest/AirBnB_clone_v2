d240a599a3ecb1d884ad1c2e52d84bcc43866ec7
wisdom <124433550+wisdomwest@users.noreply.github.com>
2023-11-20 21:31:10 +0300

    update on model files

--- models/place.py
+++ models/place.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
+from models.review import Review
 
 
 class Place(BaseModel, Base):
@@ -22,4 +23,16 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", cascade='all, delete, delete-orphan', 
+                          backref="place")
     amenity_ids = []
+    
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews."""
+            rList = []
+            for r in list(models.storage.all(Review).values()):
+                if r.place_id == self.id:
+                    rList.append(r)
+            return rList
--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -15,4 +13,6 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", cascade='all, delete, delete-orphan', 
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan', 
                           backref="user")
