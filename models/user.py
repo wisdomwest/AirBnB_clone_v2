131d61d07cc7f6a1d9a6b20a3a011d3ff98d929e
wisdomwest <harmonykoome@gmail.com>
2023-11-20 05:59:18 -0800

    added base

--- models/user.py
+++ models/user.py
@@ -4,10 +4,9 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __table_name__ = 'users'
     email = Column(String(128), nullable=False)
