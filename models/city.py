642ea68eb89ffa1667047a2acdc4dcf00f8e1958
Johnmarare <mararejohn@gmail.com>
2023-11-20 15:45:36 +0000

    updated places and User to read from db

--- models/city.py
+++ models/city.py
@@ -5,6 +5,9 @@ from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from models.place import Place
+
 
 
 class City(BaseModel, Base):
@@ -17,3 +20,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
