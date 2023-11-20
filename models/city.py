c3f7714c51db0e5204072f54b20b75b35e8caf42
wisdom <124433550+wisdomwest@users.noreply.github.com>
2023-11-20 17:20:31 +0300

    changes to models

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -17,3 +18,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
--- models/user.py
+++ models/user.py
@@ -4,12 +4,14 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __table_name__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
