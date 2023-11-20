87fbc54e34f62831f8f4ba4951c219cdf2ece9c7
wisdomwest <harmonykoome@gmail.com>
2023-11-20 10:14:32 -0800

    user.py

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
->>>>>>> origin/minion
 
 
 class User(BaseModel, Base):
