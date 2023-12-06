7864c30e9e41767415c47bc79b56567e82978c40
wisdomwest <harmonykoome@gmail.com>
2023-12-06 08:36:24 -0800

    pycode issie

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,17 +34,17 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/{}".format(path))
         run("mkdir -p /data/web_static/releases/{}/".
-           format(name))
+            format(name))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
             format(path, name))
         run("rm /tmp/{}".format(path))
         run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name))
+            "/data/web_static/releases/{}/".format(name, name))
         run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name))
+            format(name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ "
-           "/data/web_static/current".format(name))
+            "/data/web_static/current".format(name))
         return True
     except Exception as e:
         return False
