8e29ca8a7de03ce721980b818495d976ac1d9e1c
wisdomwest <harmonykoome@gmail.com>
2023-12-06 08:26:41 -0800

    confused

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,9 @@
 import os.path
 from datetime import datetime
 from fabric.api import local
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ["54.237.79.93", "34.207.63.6"]
 
@@ -12,24 +15,24 @@ def do_pack():
     """generate .tgz archive"""
     date = datetime.utcnow()
     date_format = date.strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_{}.tgz".format(date_format)
+    archive_path = "versions/web_static_{}.tgz".format(date_format)
     try:
         if not os.path.isdir("versions"):
             os.makedirs("versions")
-        local("tar -cvzf {} web_static".format(file_path))
-        return file_path
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
     except Exception as e:
         return None
 
 
-def do_deploy(file_path):
+def do_deploy(archive_path):
     """deploy to servers"""
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(archive_path):
         return False
-    path = file_path.split("/")[-1]
+    path = archive_path.split("/")[-1]
     name = path.split(".")[0]
     try:
-        put(file_path, "/tmp/{}".format(path))
+        put(archive_path, "/tmp/{}".format(path))
         run("mkdir -p /data/web_static/releases/{}/".
            format(name))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
