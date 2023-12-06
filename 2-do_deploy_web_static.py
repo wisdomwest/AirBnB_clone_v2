99ea9a336a8e5c793eb0b951c7a1b4a4f3ed9f86
wisdomwest <harmonykoome@gmail.com>
2023-12-06 07:56:47 -0800

    t

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-#using fabric to generate a tgz file
+# using fabric to generate a tgz file
+
 import os.path
 from datetime import datetime
 from fabric.api import local
+
+env.hosts = ["54.237.79.93", "34.207.63.6"]
+
 
 def do_pack():
     """generate .tgz archive"""
@@ -16,3 +20,24 @@ def do_pack():
         return file_path
     except Exception as e:
         return None
+
+
+def do_deploy(file_path):
+    """deploy to servers"""
+    if not os.path.isfile(file_path):
+        return False
+    path = file_path.split("/")[-1]
+    name = path.split(".")[0]
+    try:
+        put(file_path, "/tmp/{}".format(path))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(path, name))
+        run("rm /tmp/{}".format(path))
+        run("rm -rf /data/web_static/current")
+        run(
+            "ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current"
+        ).format(name)
+        return True
+    except Exception as e:
+        return False
