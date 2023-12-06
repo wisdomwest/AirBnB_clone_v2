67e2f2e9c03d79c6f6479dc99a1c1ce67a50849b
wisdomwest <harmonykoome@gmail.com>
2023-12-06 07:28:35 -0800

    pycode error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #using fabric to generate a tgz file
+
 import os.path
 from datetime import datetime
 from fabric.api import local
+
 
 def do_pack():
     """generate .tgz archive"""
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#using fabric to generate a tgz file
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    """generate .tgz archive"""
+    date = datetime.utcnow()
+    date_format = date.strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date_format)
+    try:
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+        local("tar -cvzf {} web_static".format(file_path))
+        return file_path
+    except Exception as e:
+        return None
