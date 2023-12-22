84d9df4cc27cbd056d09207db59c594618b3ec00
wisdomwest <harmonykoome@gmail.com>
2023-12-22 14:57:58 +0300

    task 1 added if __name__ and pycodestyle issues

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Add another route to print HBNB"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
