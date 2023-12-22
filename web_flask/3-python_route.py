d390fa03ffe39cf08baa21af96522c0e8b82e745
wisdomwest <harmonykoome@gmail.com>
2023-12-22 16:12:45 +0300

    task 2 and 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Add another route to print HBNB"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    txt = text.replace('_', ' ')
+    return f"C { txt }"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    txt = text.replace('_', ' ')
+    return f"Python { txt }"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
