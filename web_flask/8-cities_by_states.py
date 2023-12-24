3a9c13ad615d380ffe453893de473e75f40ac3ce
wisdomwest <harmonykoome@gmail.com>
2023-12-24 16:09:00 +0300

    readme

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""using db with flask to get  states list"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    data = storage.all("State")
+    return render_template('8-cities_by_states.html', data=data)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
