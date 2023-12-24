f2fdd6c6d7ce28c0e4edd6f608286fee9e5cce2a
wisdomwest <harmonykoome@gmail.com>
2023-12-24 15:49:53 +0300

    task 7 storage all returns a dict making it easier

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""using db with flask to get  states list"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    data = storage.all("State")
+    return render_template('7-states_list.html', data=data)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
